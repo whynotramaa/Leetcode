c23c317950d749050a92c3b3f1ee13b104c12d28
Rama <ramanaththakur1@gmail.com>
2026-03-08 11:09:33 +0530

    07March - BS on Answers

--- a2z/BinarySearch/07March.py
+++ a2z/BinarySearch/07March.py
@@ -123,4 +123,27 @@ def max_subarr_sum(nums, k):
     return low
 
 
-        
+def minimise_max_gap(stations, k):
+    def required(d):
+        count = 0
+        for i in range(1, len(stations)):
+            gap = stations[i] - stations[i-1]
+            count += int(gap/d)
+            if gap % d == 0:
+                count -= 1
+        return count
+    
+    low = 0
+    high = stations[-1] - stations[0]
+
+    while high-low > 1e-6:
+        mid = (low+high) / 2
+
+        if required(mid) <= k:
+            high = mid
+        else:
+            low = mid
+    
+    return high
+
+
